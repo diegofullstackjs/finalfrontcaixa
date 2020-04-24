07a5e32d01d85ea81727bf4f41c2380cb523e642
diegofullstackjs <kelvin.fabiana@gmail.com>
2020-04-24 20:11:20 +0100

    Refatorando reducer de authentificaçao

--- src/redux/reducers/Authentication.js
+++ src/redux/reducers/Authentication.js
@@ -1,12 +1,19 @@
-
-export default function Authentication(state={},action)
+const INITIAL_STATE = {
+}
+export default function Authentication(state=INITIAL_STATE,action)
 {
     switch(action.type)
     {
         case "USER_AUTHENTICATED":
             return{
                 ...state,
-                logado:true
+                logado:true,
+                user: action.payload
+            }
+        case "USER_NOT_AUTHENTICATED":
+            return {
+                logado:false,
+                message: action.payload
             }
         default:
             return state;
