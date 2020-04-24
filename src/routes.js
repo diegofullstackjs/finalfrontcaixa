6edd40dac04a5735d9968405e622d80d612f23db
diegofullstackjs <kelvin.fabiana@gmail.com>
2020-04-24 20:12:27 +0100

    refatorando rotas

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import {Switch,Route} from 'react-router-dom'
-import Header from './shared/Header'
+import LoginPage from './pages/LoginPage'
 
 export default function Routes(){
     return(
         <Switch>
-             <Route exact path="/" component={Header} />
+             <Route exact path="/" component={LoginPage} />
         </Switch>
     )
 }
