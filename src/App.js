92d525291829638811281b0f87c685877b1b730a
diegofullstackjs <kelvin.fabiana@gmail.com>
2020-04-24 20:08:39 +0100

    Conectando redux e realizando dispatch para verificar se o usuario esta logado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Routes from './routes'
+import Header from './shared/Header'
 import {BrowserRouter as Router} from 'react-router-dom'
+import {AuthLocalStorage} from './redux/actions/ActionLocalStorage'
+import {connect, useDispatch} from 'react-redux'
+import {Container} from 'react-bootstrap'
 function App() {
+  const dispatch = useDispatch()
+  useEffect(() => {
+   dispatch(AuthLocalStorage())
+  },[dispatch])
   return (
     <Router>
-      <Routes />
+      <Header />
+       <Container>
+          <Routes />
+       </Container>
     </Router>
   );
 }
-
-export default App;
+export default connect(null,null)(App);
