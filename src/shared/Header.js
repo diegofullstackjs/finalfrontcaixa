fd5efe6b1aef471531fe2008bd731540bec8ab86
diegofullstackjs <kelvin.fabiana@gmail.com>
2020-04-24 21:47:12 +0100

    Refatorando header

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {connect} from 'react-redux'
-import {Navbar,Nav,NavDropdown} from 'react-bootstrap'
+import {Navbar,Nav,NavDropdown,Container} from 'react-bootstrap'
 function Header({usuario}) {
   console.log(usuario)
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
       <Navbar.Brand href="#home">Fluxo Caixa</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -16,9 +17,8 @@ function Header({usuario}) {
           {
             usuario.logado? 
             <NavDropdown title="PAINEL" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">MEUS DADOS</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">SAIR</NavDropdown.Item>
             </NavDropdown>
             : (
               <Nav.Link href="#link" className="btn btn-primary">CADASTRA-SE</Nav.Link>
@@ -27,6 +27,7 @@ function Header({usuario}) {
 
         </Nav>
       </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
