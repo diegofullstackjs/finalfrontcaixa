edf55060ba1c35a1da8a2433ed69f1fce5c6b70f
diegofullstackjs <kelvin.fabiana@gmail.com>
2020-04-24 19:17:06 +0100

    colocando styles bootstrap

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './redux/store'
 import {Provider} from 'react-redux'
+import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
